de5da28a11a58ce1b05389877ea91c4b3d255149
Mahdy Beygi <mahdy@beygi.org>
2018-05-12 16:36:18 +0430

    add private route for restricted pages

---
+++ src/components/PrivateRoute/index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import {Redirect, Route} from "react-router";
+import {store} from "../../redux/store";
+
+const PrivateRoute = ({component: Component, ...rest}): JSX.Element => {
+    // every private page will be redirect to login page when user does not exist in store
+    return (
+        <Route {...rest} render={(props) => (
+            store.getState().app.user ? (
+                <Component {...props}/>
+            ) : (
+                <Redirect to={{
+                    pathname: "/user/login",
+                    state: {from: props.location},
+                }}/>
+            )
+        )}/>
+    );
+};
+
+export default PrivateRoute;
