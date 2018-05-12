ce7de7f86f6a7564c6c0c4cc4df1f9027aa06988
Mahdy Beygi <mahdy@beygi.org>
2018-05-12 16:37:18 +0430

    add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,59 @@
+import {Icon, Input, Layout} from "antd";
+import * as React from "react";
+
+const Search = Input.Search;
+import { connect } from "react-redux";
+import { logOut } from "../../redux/app/actions";
+import { IRootState } from "../../redux/reducers";
+
+import "./style.less";
+
+const {Header}: any = Layout;
+
+interface IProps {
+    user: any;
+    logOut: () => void;
+}
+
+interface IState {
+}
+
+class HeaderComponent extends React.Component<IProps, IState> {
+
+    constructor(props: IProps) {
+        super(props);
+
+        this.logOut = this.logOut.bind(this);
+    }
+
+    public logOut() {
+        this.props.logOut();
+    }
+
+    public render() {
+        return (
+            <Header>
+                <Icon type="logout" onClick={this.logOut} />
+                <Search
+                    placeholder="search"
+                    onSearch={(value) => console.log(value)}
+                />
+            </Header>
+        );
+    }
+
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        logOut: () => dispatch(logOut()),
+    };
+}
+
+function mapStateToProps(state: IRootState) {
+    return {
+        user: state.app.user,
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HeaderComponent);
