19b0af5eab273b107697fe7e86e8840f616498d1
Mahdy Beygi <mahdy@beygi.org>
2018-05-13 14:12:40 +0430

    add dashboard page

---
+++ src/containers/Dashboard/index.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import {connect} from "react-redux";
+import {setUser} from "../../redux/app/actions";
+import {IRootState} from "../../redux/reducers";
+
+interface IProps {
+    user: any;
+}
+
+interface IState {
+    user: any;
+}
+
+class DashboardContainer extends React.Component<IProps, IState> {
+    constructor(props: IProps) {
+        super(props);
+    }
+
+    public render() {
+        return (<div><h1>Dashboard</h1></div>);
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        user: (user) => dispatch(setUser({user})),
+    };
+}
+
+function mapStateToProps(state: IRootState) {
+    return {
+        user: state.app.user,
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashboardContainer);
