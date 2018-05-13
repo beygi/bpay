15386d23a413916ad18cebfa9d97e7062909aa34
Mahdy Beygi <mahdy@beygi.org>
2018-05-13 14:13:23 +0430

    add groups and users page

---
+++ src/containers/Groups/index.tsx
@@ -0,0 +1,159 @@
+import {Button, Icon, Input, Modal, Select, Table, Tabs, Tag} from "antd";
+import * as React from "react";
+import {connect} from "react-redux";
+import {setUser} from "../../redux/app/actions";
+import {IRootState} from "../../redux/reducers";
+import t from "../../services/trans/i18n";
+
+const Option = Select.Option;
+
+const Search = Input.Search;
+const TabPane = Tabs.TabPane;
+
+import "./style.less";
+
+interface IProps {
+    user: any;
+}
+
+interface IState {
+    // user: any;
+    visible: boolean;
+    editModal: boolean;
+}
+
+// TODO: should move to State using apis
+const columns = [
+    { title: t.t("Group Name"), dataIndex: "report", key: "report" },
+    { dataIndex: "status", key: "status", render : (record) => {
+            return {
+                children: <div>{[1, 2, 3].map((item) => <span key={item}>title,</span>)}</div>,
+                props: {
+                    className: record,
+                },
+            };
+        }, title: t.t("Permission")},
+    { dataIndex: "", key: "x", render : () => {
+            return {
+                children: <span className={"anchor"} onClick={this.showEditModal}>{t.t("edit")}</span>,
+                props: {
+                    className: "endText",
+                },
+            };
+        }, title: "" },
+];
+const data = [
+    { description: "My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.",
+        key: 1, report: t.t("lorem ipsum"),  status: "active"},
+    { description: "My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.",
+        key: 2, report: t.t("lorem ipsum"), status: "warning"},
+    { description: "My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.",
+        key: 3, report: t.t("lorem ipsum"), status: "inactive"},
+];
+const children = [];
+for (let i = 10; i < 36; i++) {
+    children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
+}
+
+class GroupsContainer extends React.Component<IProps, IState> {
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            visible: false,
+            editModal: false,
+        };
+    }
+
+    public showEditModal = () => {
+        this.setState({
+            editModal: true,
+        });
+    }
+
+    public showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    }
+    public handleOk = (e) => {
+        this.setState({
+            visible: false,
+        });
+    }
+    public handleCancel = (e) => {
+        this.setState({
+            visible: false,
+        });
+    }
+
+    public render() {
+        return (
+            <div className={"layout account-manager groups"}>
+                <Search
+                    placeholder={t.t("search")}
+                    enterButton
+                />
+                <Tabs defaultActiveKey="1">
+                    <TabPane tab={t.t("sth")} key="1">
+                        <div>
+                            <Button type={"primary"}
+                                    size={"default"} className={"addMetricBtn"}
+                                    onClick={this.showModal}>{t.t("add new group")}
+                                    </Button>
+                            <Table
+                                columns={columns}
+                                expandedRowRender={(record) => <p style={{ margin: 0 }}>{record.description}</p>}
+                                // rowClassName={(record, index) => record.status + index}
+                                dataSource={data}
+                            />
+                            <Modal
+                                title={t.t("add group")}
+                                visible={this.state.visible}
+                                onOk={this.handleOk}
+                                onCancel={this.handleCancel}
+                            >
+                                <Input placeholder={t.t("Group Name")} />
+                                <Select
+                                    mode="multiple"
+                                    className={"fullWidth"}
+                                    placeholder="Permission"
+                                >
+                                    {children}
+                                </Select>
+                            </Modal>
+                            <Modal
+                                title={t.t("edit group")}
+                                visible={this.state.editModal}
+                                onOk={this.handleOk}
+                                onCancel={this.handleCancel}
+                            >
+                                <Input placeholder={t.t("Group Name")} />
+                                <Select
+                                    mode="multiple"
+                                    className={"fullWidth"}
+                                    placeholder="Permission"
+                                >
+                                    {children}
+                                </Select>
+                            </Modal>
+                        </div>
+                    </TabPane>
+                </Tabs>
+            </div>
+        );
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        user: (user) => dispatch(setUser({user})),
+    };
+}
+
+function mapStateToProps(state: IRootState) {
+    return {
+        user: state.app.user,
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GroupsContainer);
---
+++ src/containers/Users/index.tsx
@@ -0,0 +1,227 @@
+import * as React from "react";
+import {connect} from "react-redux";
+import API from "../../lib/api/index";
+import {setUser} from "../../redux/app/actions";
+import {IRootState} from "../../redux/reducers";
+
+import {Button, Input, Popover, Table, Tabs, Tag} from "antd";
+
+const Search = Input.Search;
+const TabPane = Tabs.TabPane;
+const {CheckableTag} = Tag;
+import paginationElement from "../../components/Pagination";
+
+import t from "../../services/trans/i18n";
+
+import "./style.less";
+
+interface IProps {
+    user: any;
+}
+
+interface IState {
+    users: any[];
+    loading: boolean;
+    pagination: any;
+}
+
+const columns = [
+    {
+        dataIndex: "vip", render: (record) => {
+            return {
+                children: record === "vip" ? "VIP" : "",
+                props: {
+                    className: record === "vip" ? "vip" : "",
+                },
+            };
+        }, title: "",
+    },
+    {
+        dataIndex: "fullname", render: (record) => {
+            console.log(record);
+            return {
+                children: record,
+                props: {
+                    className: "noRightPadding",
+                },
+            };
+        }, title: "Full Name",
+    },
+    {
+        dataIndex: "role", render: (record) => {
+            return {
+                children: record,
+                props: {
+                    className: "role",
+                },
+            };
+        }, title: "Role",
+    },
+    {title: "Skill", dataIndex: "skillName", key: "skillName"},
+    {
+        dataIndex: "status", key: "status", render: (record) => {
+            return {
+                children: <span><i className={"circle"}/>
+                    <Popover content={content} trigger="click">{record}</Popover></span>,
+                props: {
+                    className: record,
+                },
+            };
+        }, title: "Status",
+    },
+    {
+        dataIndex: "", key: "x", render: () => {
+            return {
+                children: <a href="#">{t.t("view profile")}</a>,
+                props: {
+                    className: "endText",
+                },
+            };
+        }, title: "",
+    },
+];
+
+const content = (
+    <div>
+        <Button type="primary">{t.t("active")}</Button>
+        <Button>{t.t("verify")}</Button>
+        <Button>{t.t("test")}</Button>
+    </div>
+);
+
+class CustomersContainer extends React.Component<IProps, IState> {
+    private api = API.getInstance();
+    private skills = [];
+
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            loading: true,
+            users: [],
+            pagination: {},
+        };
+    }
+
+    public componentDidMount() {
+        this.loadData();
+    }
+
+    public render() {
+        return (
+            <div className={"layout account-manager users"}>
+                <Search
+                    placeholder={t.t("search")}
+                    onSearch={
+                        (value) => {
+                            // console.log(value)
+                        }
+                    }
+                    enterButton
+                />
+                <Tabs defaultActiveKey="1">
+                    <TabPane tab="Tab 1" key="1">
+                        <div className={"tag-box"}>
+                            <CheckableTag checked={false}>{t.t("inactive")}</CheckableTag>
+                            <CheckableTag checked={true}>{t.t("active")}</CheckableTag>
+                            <CheckableTag checked={false}>{t.t("warning")}</CheckableTag>
+                            <CheckableTag checked={false}>{t.t("in progress")}</CheckableTag>
+                        </div>
+                        <div className={"chart-box"}>
+                        </div>
+                        <Table
+                            loading={this.state.loading}
+                            columns={columns}
+                            expandedRowRender={(record) => <p style={{margin: 0}}>{record.description}</p>}
+                            // rowClassName={(record, index) => record.status + index}
+                            dataSource={this.state.users}
+                            pagination={this.state.pagination}
+                            onChange={this.tableChange}
+                        />
+                    </TabPane>
+                </Tabs>
+            </div>
+        );
+    }
+
+    private tableChange = (pagination, filters, sorter) => {
+        this.setState({
+            pagination,
+            loading: true,
+        });
+        this.loadData(pagination.current);
+    }
+
+    private loadSkills(force: boolean = false): Promise<any> {
+        return new Promise<any>((res, rej) => {
+
+            if (this.skills.length > 0 && !force) {
+                res(this.skills);
+                return;
+            }
+
+            // this.api.skills_list_get({pageNumber: 1, pageSize: 100})
+            //     .then((skills) => {
+            //         this.skills = this.skills.concat(skills);
+            //         res(this.skills);
+            //     });
+        });
+    }
+
+    private getSkillName(skillId: number): string {
+        const skill = this.skills.filter((s) => {
+            return (s.id === skillId);
+        });
+        if (skill && skill[0]) {
+            return skill[0].title;
+        }
+        return "";
+    }
+
+    private loadData(page = 1) {
+        const promises: any = [];
+        // promises.push(this.loadSkills(false));
+        // promises.push(this.api.users_list_get({pageNumber: page, pageSize: 15}, false));
+
+        Promise.all(promises)
+            .then((values) => {
+                const skills = values[0];
+                const users: any = values[1];
+                this.setState({
+                    pagination: paginationElement(users.links),
+                    users: users.data.map((user) => {
+                        const userObj: {
+                            [key: string]: any;
+                        } = {};
+                        userObj.id = user.id;
+                        userObj.fullname = user.attributes.full_name;
+                        userObj.role = user.attributes.role;
+                        userObj.status = user.attributes.status;
+                        userObj.email = user.attributes.email;
+                        userObj.skillName = this.getSkillName(user.relationships.skill.data.id);
+                        return userObj;
+                    }),
+                    loading: false,
+                });
+            })
+            .catch(() => {
+                this.setState({
+                    users: [],
+                    loading: false,
+                });
+            });
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        user: (user) => dispatch(setUser({user})),
+    };
+}
+
+function mapStateToProps(state: IRootState) {
+    return {
+        user: state.app.user,
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CustomersContainer);
