64ce18894fd43574604795dbdba400b4ab3b1bbf
Mahdy Beygi <mahdy@beygi.org>
2018-05-14 16:29:14 +0430

    update keycloak config

--- src/config.tsx
+++ src/config.tsx
@@ -2,13 +2,11 @@ import Languages from "./services/trans/languages";
 const config = {
     apiUrl: "http://127.0.0.1:80/",
     language: Languages.en,
-    keycloakConfig: {
-        "realm": "master",
-        "auth-server-url": "https://51.38.197.75/auth",
-        "ssl-required": "external",
-        "resource": "webapp",
-        "public-client": true,
-        "confidential-port": 0,
+    keycloakConfig : {
+        url: "https://51.38.197.75/auth",
+        realm: "master",
+        clientId: "webapp",
+        checkLoginIframe: false,
     },
 };
 
