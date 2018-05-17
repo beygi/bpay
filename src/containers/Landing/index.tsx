89287f6a78cc3e29885d0cdcdba856212457217e
Mahdy Beygi <mahdy@beygi.org>
2018-05-17 14:13:30 +0430

    heavy change on style structures

--- src/containers/Landing/index.tsx
+++ src/containers/Landing/index.tsx
@@ -2,8 +2,12 @@ import { Button } from "antd";
 import * as React from "react";
 import { Redirect } from "react-router";
 import USER from "./../../lib/user";
+import "./style.less";
+const logo = require("../../assets/images/logo.png");
 
-const user =  USER.getInstance();
+const user = USER.getInstance();
+import { Breadcrumb, Layout, Menu } from "antd";
+const { Header, Footer, Sider, Content } = Layout;
 
 interface IProps {
 }
@@ -24,18 +28,32 @@ class LandingContainer extends React.Component<IProps, IState> {
             );
         }
         return (
-            <div>
-                <h1>Landing</h1>
-                <Button onClick={this.login} type="primary" htmlType="submit" className="login-form-button">
-                    Login
-                </Button>
-            </div>
-        );
-    }
+
+            <Layout>
+                <div className="header-logo">
+                    <img src={logo} />
+                </div>
+                    <Header className="header">
+                        <Menu
+                            theme="dark"
+                            mode="horizontal"
+                            defaultSelectedKeys={["2"]}
+                            style={{ lineHeight: "64px" }}
+                        >
+                            <Menu.Item key="1">nav 1</Menu.Item>
+                            <Menu.Item key="2">nav 2</Menu.Item>
+                            <Menu.Item key="3">nav 3</Menu.Item>
+                        </Menu>
+                    </Header>
+                    <Content>Content</Content>
+                    <Footer>Footer</Footer>
+</Layout>
+                );
+                    }
 
     private login() {
-        user.keycloak.login();
-    }
-}
+                    user.keycloak.login();
+                }
+            }
 
 export default LandingContainer;
