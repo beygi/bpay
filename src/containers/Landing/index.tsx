8f45e5ba77eea97d36ff524f34e8be560bcbba54
Mahdy Beygi <mahdy@beygi.org>
2018-05-20 18:39:23 +0430

    add some sample photo after hero content

--- src/containers/Landing/index.tsx
+++ src/containers/Landing/index.tsx
@@ -5,8 +5,9 @@ import "./style.less";
 const logo = require("../../assets/images/logo.png");
 
 const user = USER.getInstance();
-import { Breadcrumb, Button, Icon , Layout, Menu } from "antd";
+import { Breadcrumb, Button, Card, Col, Icon, Layout, Menu, Row } from "antd";
 const { Header, Footer, Sider, Content } = Layout;
+const { Meta } = Card;
 
 interface IProps {
 }
@@ -40,18 +41,43 @@ class LandingContainer extends React.Component<IProps, IState> {
                 </div>
                 <div className="landing-content">
                     <h1>B2B MARKET PLACE</h1>
-                         <div className="login-signin">
-                              <Button onClick={this.register} className="startButton" type="primary" size="large">
-                                   Get Started <Icon type="right" />
-                              </Button>
-                              <br />
-                              <div className="already-memeber">
-                                  already memeber?
+                    <div className="login-signin">
+                        <Button onClick={this.register} className="startButton" type="primary" size="large">
+                            Get Started <Icon type="right" />
+                        </Button>
+                        <br />
+                        <div className="already-memeber">
+                            already memeber?
                                       <a className="signin" onClick={this.login}>sign in</a>
-                              </div>
-                          </div>
+                        </div>
+                    </div>
                 </div>
                 <Footer>
+
+                    <Row  gutter={16}>
+                        <Col xs={24} md={8}>
+
+                            <Card
+                                cover={<img alt="example" src="https://static.giantbomb.com/uploads/scale_super/9/93998/2238069-tfjlg.png" />}
+                                hoverable title="Card title" >
+                            </Card>
+                        </Col>
+                        <Col xs={24} md={8}>
+
+                            <Card
+                                cover={<img alt="example" src="https://i.pinimg.com/originals/17/5e/ce/175ececbf3de2a05b400178d478aa075.jpg" />}
+                                hoverable title="Card title" >
+                            </Card>
+                        </Col>
+                        <Col xs={24} md={8}>
+
+                            <Card
+                                cover={<img alt="example" src="http://2.bp.blogspot.com/-ZxJraKZRso4/T9_6vUVbjVI/AAAAAAAAGEs/f2eaWi5OHow/s1600/W&HM+-+Tekken+Girls+@+2012+E3+(244).JPG" />}
+                                hoverable title="Card title" >
+                            </Card>
+                        </Col>
+                    </Row>
+
                     Hello, we are the worldwide stock exchange. A market where
                      anyone from anywhere can invest in any business worldwide
                       using cryptocurrencies (BTC, ETH, NXT, and others) or fiat money.
