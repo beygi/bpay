dc694546c41fcb16fcadf4d89b7744680e82ce77
Mahdy Beygi <mahdy@beygi.org>
2018-05-21 11:52:02 +0430

    landing page compeleted

--- src/containers/Landing/index.tsx
+++ src/containers/Landing/index.tsx
@@ -4,8 +4,12 @@ import USER from "./../../lib/user";
 import "./style.less";
 const logo = require("../../assets/images/logo.png");
 
+const home1 = require("../../assets/images/home1.png");
+const home2 = require("../../assets/images/home2.png");
+const home3 = require("../../assets/images/home3.png");
+
 const user = USER.getInstance();
-import { Breadcrumb, Button, Card, Col, Icon, Layout, Menu, Row } from "antd";
+import { Breadcrumb, Button, Card, Col, Icon, Layout, Menu, Row, Tooltip } from "antd";
 const { Header, Footer, Sider, Content } = Layout;
 const { Meta } = Card;
 
@@ -35,12 +39,13 @@ class LandingContainer extends React.Component<IProps, IState> {
                     <img src={logo} />
                 </div>
                 <div className="top-menu">
+                    <a className="exchange" href="">Exchange</a>
                     <a href="">FAQ</a>
                     <a href="">SUPPORT</a>
                     <a href="">ABOUT US</a>
                 </div>
                 <div className="landing-content">
-                    <h1>B2B MARKET PLACE</h1>
+                    <h1>B2B Market Place</h1>
                     <div className="login-signin">
                         <Button onClick={this.register} className="startButton" type="primary" size="large">
                             Get Started <Icon type="right" />
@@ -52,40 +57,72 @@ class LandingContainer extends React.Component<IProps, IState> {
                         </div>
                     </div>
                 </div>
-                <Footer>
+                <Content className="hero">
 
-                    <Row  gutter={16}>
+                    <Row gutter={16}>
                         <Col xs={24} md={8}>
 
                             <Card
-                                cover={<img alt="example" src="https://static.giantbomb.com/uploads/scale_super/9/93998/2238069-tfjlg.png" />}
-                                hoverable title="Card title" >
+                                cover={<img alt="example" src={home1} />}
+                                hoverable title="Fast Trading" >
                             </Card>
                         </Col>
                         <Col xs={24} md={8}>
 
                             <Card
-                                cover={<img alt="example" src="https://i.pinimg.com/originals/17/5e/ce/175ececbf3de2a05b400178d478aa075.jpg" />}
-                                hoverable title="Card title" >
+                                cover={<img alt="example" src={home2} />}
+                                hoverable title="Find Best Sellers" >
                             </Card>
                         </Col>
                         <Col xs={24} md={8}>
 
                             <Card
-                                cover={<img alt="example" src="http://2.bp.blogspot.com/-ZxJraKZRso4/T9_6vUVbjVI/AAAAAAAAGEs/f2eaWi5OHow/s1600/W&HM+-+Tekken+Girls+@+2012+E3+(244).JPG" />}
-                                hoverable title="Card title" >
+                                cover={<img alt="example" src={home3} />}
+                                hoverable title="Buy high quality products" >
                             </Card>
                         </Col>
                     </Row>
+                </Content>
+                <Footer>
+                    <Row gutter={16}>
+                        <Col sm={24} md={12}>
+                            <h1><Icon type="user" /> Who we are ?</h1>
+                            Hello, we are the worldwide stock exchange. A market where
+                             anyone from anywhere can invest in any business worldwide
+                              using cryptocurrencies (BTC, ETH, NXT, and others) or fiat money.
+                            </Col>
+                        <Col sm={24} md={12}>
+                            <h1><Icon type="wallet" /> What we do ?</h1>
+                            Imagine you can invest in Chinese manufacturing or a Brazilian sawmill
+                             without papers in just minutes. Imagine anybody can invest in your business too.
+                              On this worldwide stock exchange market, businesses can issue their stocks, assets,
+                               or tokens on major established crypto platforms (NXT, ARDOR, ETH, etc.), sell them,
+                                do business, and pay dividends to stock holders in just one click.ney.
+                            </Col>
+                    </Row>
+                    <Row className="socials" type="flex" justify="center" gutter={16}>
+                        <Col md={2}>
+                            <Tooltip placement="top" title="Twitter">
+                                <a href=""><Icon type="twitter" /></a>
+                            </Tooltip>
+                        </Col>
+                        <Col md={2}>
+                            <Tooltip placement="top" title="Email">
+                                <a href=""><Icon type="mail" /></a>
+                            </Tooltip>
+                        </Col>
+                        <Col md={2}>
+                            <Tooltip placement="top" title="Api Doc">
+                                <a href=""><Icon type="api" /></a>
+                            </Tooltip>
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col className="copyright" md={24}>
+                            B2B Corporation © 2018
+                         </Col>
+                    </Row>
 
-                    Hello, we are the worldwide stock exchange. A market where
-                     anyone from anywhere can invest in any business worldwide
-                      using cryptocurrencies (BTC, ETH, NXT, and others) or fiat money.
-                      Imagine you can invest in Chinese manufacturing or a Brazilian sawmill
-                       without papers in just minutes. Imagine anybody can invest in your business too.
-                        On this worldwide stock exchange market, businesses can issue their stocks, assets,
-                         or tokens on major established crypto platforms (NXT, ARDOR, ETH, etc.), sell them,
-                          do business, and pay dividends to stock holders in just one click.
                 </Footer>
             </Layout>
         );
