d6ea1201c36bd03daa7d31dc408005015f9096e1
Mahdy Beygi <mahdy@beygi.org>
2018-05-31 18:11:50 +0430

    add style for notification system in dashboard

--- src/components/UserStatusGuide/index.tsx
+++ src/components/UserStatusGuide/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Icon, Row, Steps, Tag } from "antd";
+import { Button, Col, Icon, notification, Row, Steps } from "antd";
 import * as React from "react";
 
 import { connect } from "react-redux";
@@ -21,23 +21,35 @@ class UserStatusGuideComponent extends React.Component<IProps, IState> {
 
     constructor(props: IProps) {
         super(props);
+
+        // this.action = this.action.bind(this);
     }
 
     public render() {
         return (
             <div className="user-status-guide">
                 <Steps>
-                  <Step description={t.t("You are a member")}  status="finish" title={t.t("Register")} icon={<Icon type="user" />} />
-                  <Step description={t.t("Verify your email address")} status="finish" title={t.t("Verify email")}  icon={<Icon type="mail" />} />
-                 <Step description={t.t("Checking documents")} status="process" title={t.t("Check")} icon={<Icon type="loading" />} />
-                  <Step description="This is a description." status="wait" title={t.t("Identity check")} icon={<Icon type="solution" />} />
-                  <Step description={t.t("Happy Trading")}  status="wait" title="Done" icon={<Icon type="check" />} />
+                    <Step description={t.t("You are a member")} status="finish" title={t.t("Register")} icon={<Icon type="user" />} />
+                    <Step description={t.t("Verify your email address")} status="finish" title={t.t("Verify email")} icon={<Icon type="mail" />} />
+                    <Step description={t.t("Checking documents")} status="process" title={t.t("Check")} icon={<Icon type="loading-3-quarters" />} />
+                    <Step description="This is a description." status="wait" title={t.t("Identity check")} icon={<Icon type="solution" />} />
+                    <Step description={t.t("Happy Trading")} status="wait" title="Done" icon={<Icon type="check" />} />
                 </Steps>
-                    <div>
-                            <Button className="action"  icon="mail"  type="primary" size="large">Verify Email Address </Button>
-                    </div>
+                <div>
+                    <Button onClick={() => this.action("success")}  className="action" icon="mail" type="primary" size="large">Verify Email Address </Button>
+                </div>
             </div >
         );
+    }
+
+    private action(type) {
+        // success info  warning error
+        notification.success({
+            message: "Notification Title",
+            description: "This is the content of the notification. This is the content of the notification. This is the content of the notification.",
+            placement : "bottomRight",
+            duration : 0,
+        });
     }
 
 }
