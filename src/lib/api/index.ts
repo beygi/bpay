674a0c440d3bb34340e66b35ea6a2495402c2cac
Mahdy Beygi <mahdy@beygi.org>
2018-06-18 10:33:42 +0430

    add axios and headers/base url in api

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,50 +1,52 @@
-import config from "../../../src/config"
+import axios from "axios";
+import config from "../../../src/config";
 
 export default class API {
-    private static instance: API;
-    private baseURL: string = window.location.host + "/";
-    private AuthToken: string | null;
-    private Company: string | null;
-
-    private constructor() {
-        // empty;
-        this.setBaseURL(config.apiUrl);
-    }
-
     public static getInstance() {
         if (!this.instance) {
             this.instance = new API();
         }
         return this.instance;
     }
+    private static instance: API;
+
+    private baseURL: string;
+    private AuthToken: string | null;
+    private axios: any;
+
+    private constructor() {
+        // empty;
+        this.setBaseURL(config.apiUrl);
+    }
+
+    public CreateAxiosInstance() {
+        this.axios = axios.create({
+            baseURL: this.baseURL,
+            timeout: 1000,
+            headers: this.getHeaders(),
+        });
+    }
 
     public setBaseURL(url: string) {
         this.baseURL = url;
+        this.CreateAxiosInstance();
     }
 
     public setAuthToken(token: string) {
         this.AuthToken = token;
-    }
-
-    public setCompany(company: string) {
-        this.Company = company;
+        this.CreateAxiosInstance();
     }
 
     public destroy() {
-        this.Company = null;
         this.AuthToken = null;
     }
 
     private getHeaders(): any {
-        let headers = new Headers();
+        const headers = new Headers();
         headers.append("Content-Type", "application/json");
         headers.append("Accept-Language", "fa_IR");
-
         if (this.AuthToken) {
-            headers.append("X-Auth-Token", this.AuthToken);
-        }
-        if (this.Company) {
-            headers.append("X-Company", this.Company);
+            headers.append("Authorisation", "Token " + this.AuthToken);
         }
         return headers;
     }
