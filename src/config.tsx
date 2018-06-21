56e800acafe88035cd5fd7fc1879ba9212354312
Mahdy Beygi <mahdy@beygi.org>
2018-06-21 17:29:25 +0430

    add server url to api file

--- src/config.tsx
+++ src/config.tsx
@@ -1,6 +1,7 @@
 import Languages from "./services/trans/languages";
 const config = {
-    apiUrl: "http://192.168.1.42:9092/",
+    // apiUrl: "http://192.168.1.42:9092/",
+    apiUrl: "http://87.98.188.77:9092/",
     language: Languages.en,
     keycloakConfig : {
         url: "https://51.38.197.75/auth",
