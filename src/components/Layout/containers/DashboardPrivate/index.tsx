79fe58f8ce1b2287ee2440d544b4aec99eaad756
Mahdy Beygi <mahdy@beygi.org>
2018-06-27 12:37:25 +0430

    add scroll event for handling mini-menu

--- src/components/Layout/containers/DashboardPrivate/index.tsx
+++ src/components/Layout/containers/DashboardPrivate/index.tsx
@@ -15,6 +15,7 @@ interface IProps {
 
 interface IState {
     collapsed: boolean;
+    menuClass: string; // normal or mini
 }
 
 export default class DashboardPrivateLayout extends React.Component<IProps, IState> {
@@ -22,16 +23,39 @@ export default class DashboardPrivateLayout extends React.Component<IProps, ISta
         super(props);
 
         this.toggle = this.toggle.bind(this);
+        this.handleScroll = this.handleScroll.bind(this);
 
         this.state = {
             collapsed: false,
+            menuClass: "normal-menu",
         };
     }
 
+    public componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
+    }
+
+    public componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+
+public handleScroll(event) {
+        console.log(event.pageY);
+        if (event.pageY > 25) {
+             this.setState({
+               menuClass: "mini-menu",
+             });
+        } else {
+            this.setState({
+              menuClass: "normal-menu",
+            });
+        }
+    }
+
     public render() {
-        let AdminClass: string = "";
+        let AdminClass: string = this.state.menuClass;
         if (this.props.isAdmin) {
-            AdminClass = "admin-menu";
+            AdminClass = "admin-menu " + this.state.menuClass;
         }
         return (
             <Layout className={AdminClass} style={{ minHeight: "100vh" }}>
