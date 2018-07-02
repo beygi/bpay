56206c428e427be8598012488ab1bc714c949d03
Mahdy Beygi <mahdy@beygi.org>
2018-07-02 17:45:21 +0430

    temporary remove beautifier to debug

--- src/lib/swager/generate.js
+++ src/lib/swager/generate.js
@@ -3,7 +3,7 @@ var CodeGen = require('swagger-typescript-codegen').CodeGen;
 
 var file = './api-docs.json';
 var swagger = JSON.parse(fs.readFileSync(file, 'UTF-8'));
-var tsSourceCode = CodeGen.getTypescriptCode({ className: 'B2Mark', swagger: swagger });
+var tsSourceCode = CodeGen.getTypescriptCode({ className: 'B2Mark', swagger: swagger , beautify : false });
 
 fs.writeFile(process.argv[2], tsSourceCode, function(err) {
     if(err) {
