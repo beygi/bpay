d92ebe5db18a443c60b9557126d8bdffd336dc20
Mahdy Beygi <mahdy@beygi.org>
2018-07-02 16:58:34 +0430

    generator now writes output to file

--- src/lib/swager/generate.js
+++ src/lib/swager/generate.js
@@ -4,4 +4,11 @@ var CodeGen = require('swagger-typescript-codegen').CodeGen;
 var file = './api-docs.json';
 var swagger = JSON.parse(fs.readFileSync(file, 'UTF-8'));
 var tsSourceCode = CodeGen.getTypescriptCode({ className: 'B2Mark', swagger: swagger });
-console.log(tsSourceCode);
+
+fs.writeFile(process.argv[2], tsSourceCode, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+});
