fec8b1446bcacfa8437cf92839ff8d260f8cf108
Mahdy Beygi <mahdy@beygi.org>
2018-07-03 14:20:44 +0430

    add new property for new api lib in config file

--- src/config.tsx
+++ src/config.tsx
@@ -2,6 +2,7 @@ import Languages from "./services/trans/languages";
 const config = {
     // apiUrl: "http://192.168.1.42:9092/",
     apiUrl: "http://87.98.188.77:9092/",
+    NewApiUrl: "http://87.98.188.77:9092",
     language: Languages.en,
     keycloakConfig : {
         url: "https://aas.avazcoin.com/auth",
