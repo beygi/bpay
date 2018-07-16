5f126e7f5c1b0cd407595cdff2a2dc0797bcab92
Mahdy Beygi <mahdy@beygi.org>
2018-07-16 15:08:56 +0430

    move generated apis to new place

--- src/lib/api/generate.js
+++ src/lib/api/generate.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var CodeGen = require('swagger-typescript-codegen').CodeGen;
 
-var file = "./"+process.argv[2]
+var file = "./swager-specs/"+process.argv[2]
 // './api-docs.json';
 
 
@@ -9,7 +9,7 @@ var file = "./"+process.argv[2]
 var swagger = JSON.parse(fs.readFileSync(file, 'UTF-8'));
 var tsSourceCode = CodeGen.getTypescriptCode({ className: process.argv[2].split(".")[0]+"Api", swagger: swagger , beautify : true });
 
-fs.writeFile("./src/lib/swager/"+ process.argv[2]+".ts", tsSourceCode, function(err) {
+fs.writeFile("./src/lib/api/"+ process.argv[2]+".ts", tsSourceCode, function(err) {
     if(err) {
         return console.log(err);
     }
