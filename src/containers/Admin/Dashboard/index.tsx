c9cf3602d1c6a01e4240e82bb306ba31581cbaf2
Mahdy Beygi <mahdy@beygi.org>
2018-07-18 14:07:04 +0430

    add example cashDesks to admin panel

--- src/containers/Admin/Dashboard/index.tsx
+++ src/containers/Admin/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import { Col, Layout, Row } from "antd";
 import * as React from "react";
 import {connect} from "react-redux";
+import Balance from "../../../components/Balance";
 import Profile from "../../../components/DashboardHeaderProfile";
 import Block from "../../../components/Holder";
 import Guide from "../../../components/UserStatusGuide";
 import {setUser} from "../../../redux/app/actions";
 import {IRootState} from "../../../redux/reducers";
+import t from "../../../services/trans/i18n";
 
 interface IProps {
     user: any;
@@ -32,12 +34,31 @@ class AdminDashboardContainer extends React.Component<IProps, IState> {
                   </Block>
               </Col>
               <Col md={18} >
+                  <Row gutter={8}>
+                        <Col md={6} >
+                            <Block title={t.t("Hot storage")} >
+                                <Balance />
+                            </Block>
+                        </Col>
+                        <Col md={6} >
+                            <Block collapse title={t.t("Cold storage")} >
+                                <Balance />
+                            </Block>
+                        </Col>
+                        <Col md={6} >
+                            <Block collapse title={t.t("Tax Cash Desk")} >
+                                <Balance />
+                            </Block>
+                        </Col>
+                        <Col md={6} >
+                            <Block collapse title={t.t("Fee Cash Desk")} >
+                                <Balance />
+                            </Block>
+                        </Col>
+                  </Row>
                    <Block>
-                       <img src="https://dummyimage.com/1000x300/4c4649/3ee6e0.png" alt=""/>
+                       <img src="https://dummyimage.com/100x300/4c4649/3ee6e0.png" alt=""/>
                    </Block>
-                    <Block>
-                            <img src="https://dummyimage.com/1000x400/4c4649/3ee6e0.png" alt=""/>
-                    </Block>
               </Col>
             </Row>
         );
