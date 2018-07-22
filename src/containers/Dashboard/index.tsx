34b32a40fec91006b5dc0535299815ff322e3d39
Mahdy Beygi <mahdy@beygi.org>
2018-07-22 15:46:08 +0430

    add deposite button to users balance block

--- src/containers/Dashboard/index.tsx
+++ src/containers/Dashboard/index.tsx
@@ -1,13 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Col, Layout, Row } from "antd";
+import { Button, Col, Layout, Row } from "antd";
 import * as React from "react";
 import {connect} from "react-redux";
+import { Link } from "react-router-dom";
 import Balance from "../../components/Balance";
 import Profile from "../../components/DashboardHeaderProfile";
 import Block from "../../components/Holder";
 import Guide from "../../components/UserStatusGuide";
 import {setUser} from "../../redux/app/actions";
 import {IRootState} from "../../redux/reducers";
+import "./style.less";
 
 interface IProps {
 }
@@ -24,8 +26,11 @@ class DashboardContainer extends React.Component<IProps, IState> {
         return (
             <Row gutter={8}>
               <Col md={6} >
-                  <Block collapse title="Balance" icon={<FontAwesomeIcon  icon={ ["fas", "balance-scale"] }  />} iconPosition="right" >
+                  <Block className="user-balance" collapse title="Balance" icon={<FontAwesomeIcon  icon={ ["fas", "balance-scale"] }  />} iconPosition="right" >
                       <Balance />
+                      <Link to="/deposite">
+                          <Button  className="action"  type="primary" size="small">Deposite</Button>
+                      </Link>
                   </Block>
                   <Block>
                           <img src="https://dummyimage.com/600x800/4c4649/3ee6e0.png" alt=""/>
