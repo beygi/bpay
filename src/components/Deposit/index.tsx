e87c601d71a7ed0f5e643845829844d9dbc994ee
Mahdy Beygi <mahdy@beygi.org>
2018-07-25 17:01:41 +0430

    working on show deposite wallet

--- src/components/Deposit/index.tsx
+++ src/components/Deposit/index.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Alert } from "antd";
+import { Alert, Button, Input, List, Tooltip } from "antd";
 import * as React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
@@ -12,6 +12,7 @@ import USER from "./../../lib/user";
 import "./style.less";
 
 const userObject = USER.getInstance();
+const InputGroup = Input.Group;
 
 interface IProps {
     selectedDepositCurrency: string;
@@ -33,21 +34,59 @@ class DepositComponent extends React.Component<IProps, IState> {
             message={t.t("Before you start")}
             description={<div>Additional description and informations about deposit
             <ul>
-                <li>Additional description and informations about deposit </li>
-                <li>Additional description and informations about deposit </li>
-                <li>Additional description and informations about deposit </li>
-                <li>Additional description and informations about deposit </li>
-                <li>Additional description and informations about deposit </li>
-            </ul>
+                    <li>Additional description and informations about deposit </li>
+                    <li>Additional description and informations about deposit </li>
+                    <li>Additional description and informations about deposit </li>
+                    <li>Additional description and informations about deposit </li>
+                    <li>Additional description and informations about deposit </li>
+                </ul>
             </div>}
-        type = "info"
-        showIcon
-        / > ;
+            type="info"
+            showIcon
+        />;
         let collapseClosed = false;
         if (this.props.selectedDepositCurrency) {
+            const data = [
+                t.t("Total"),
+                t.t("In order"),
+                t.t("Available"),
+            ];
             const CurrencydropDownIcon = <FontAwesomeIcon icon={config.currencies[this.props.selectedDepositCurrency].icon} />;
             dropDownName = <div> {CurrencydropDownIcon} {config.currencies[this.props.selectedDepositCurrency].name}</div>;
-            DepositOrDescription = <div>{config.currencies[this.props.selectedDepositCurrency].name}</div>;
+            DepositOrDescription = <div>
+                <h3>your {this.props.selectedDepositCurrency} balance:</h3>
+                <List className="deposite-balance" bordered={false}
+                    size="small"
+                    dataSource={data}
+                    renderItem={(item) => (<List.Item><span className="deposite-balance">{item}</span>20</List.Item>)}
+                />
+                <div className="wallet-label">Our {config.currencies[this.props.selectedDepositCurrency].name} wallet address:</div>
+                <InputGroup className="wallet-group" compact>
+                    <Input className="wallet" defaultValue={config.currencies[this.props.selectedDepositCurrency].depositeWallet} />
+                    <Tooltip placement="top" title="Copy wallet address to clipboard">
+                                <Button className="copy neat-btn" type="primary"><FontAwesomeIcon icon={["fas", "copy"]} />  Copy</Button>
+                    </Tooltip>
+                    <Tooltip placement="top" title="Display Qr code of wallet">
+                                <Button className="show-qr neat-btn" type="primary"><FontAwesomeIcon icon={["fas", "qrcode"]} />  Qr code</Button>
+                    </Tooltip>
+
+        </InputGroup>
+
+        <Alert
+            message={t.t("Warning")}
+            description={<div>Additional description and informations about deposit
+            <ul>
+                    <li>Additional description and informations about deposit </li>
+                    <li>Additional description and informations about deposit </li>
+                    <li>Additional description and informations about deposit </li>
+                    <li>Additional description and informations about deposit </li>
+                    <li>Additional description and informations about deposit </li>
+                </ul>
+            </div>}
+            type="warning"
+            showIcon
+        />
+            </div>;
             collapseClosed = true;
         }
         const coins = Object.keys(config.currencies).map((key) =>
@@ -59,7 +98,7 @@ class DepositComponent extends React.Component<IProps, IState> {
             </Link>,
         );
         if (this.props.selectedDepositCurrency) {
-            return (<div><Block className="deposit-coin-select" centerTitle={true} title={dropDownName}  icon={<Link replace={true} to={`/deposit/`} >{dropDownIcon}</Link>} >
+            return (<div><Block className="deposit-coin-select" centerTitle={true} title={dropDownName} icon={<Link replace={true} to={`/deposit/`} >{dropDownIcon}</Link>} >
             </Block>
                 {DepositOrDescription}
             </div>
--- src/lib/icon/index.ts
+++ src/lib/icon/index.ts
@@ -1,9 +1,9 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faBtc, faEthereum, faGripfire } from "@fortawesome/free-brands-svg-icons";
 import {
-    faAddressCard, faAngleDown, faBalanceScale, faBox, faDollarSign, faDonate, faExchangeAlt, faHandHoldingUsd, faHandshake, faHistory,
-    faMoneyCheckAlt, faSearch, faSnowflake, faWallet,
+    faAddressCard, faAngleDown, faBalanceScale, faBox, faCopy, faDollarSign, faDonate, faExchangeAlt, faHandHoldingUsd, faHandshake,
+    faHistory, faMoneyCheckAlt, faQrcode, faSearch, faSnowflake, faWallet,
 } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faSearch, faWallet, faHistory, faBtc, faDollarSign, faEthereum, faBox, faBalanceScale, faExchangeAlt, faGripfire, faSnowflake,
-    faDonate, faHandHoldingUsd, faMoneyCheckAlt, faHandshake, faAddressCard, faAngleDown);
+    faDonate, faHandHoldingUsd, faMoneyCheckAlt, faHandshake, faAddressCard, faAngleDown, faQrcode, faCopy);
