c8ca0cb87bf2617e9319a0f89fea1285ff2c0949
Mahdy Beygi <mahdy@beygi.org>
2018-08-02 12:59:36 +0430

    add sessions component with dummy data

---
+++ src/components/Sessions/index.tsx
@@ -0,0 +1,67 @@
+// use this dummy component to create new components
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Table } from "antd";
+import * as React from "react";
+import t from "../../services/trans/i18n";
+import "./style.less";
+
+interface IProps {
+}
+
+interface IState {
+}
+
+const columns = [{
+  title: "IP",
+  dataIndex: "ip",
+}, {
+  title: "Started",
+  dataIndex: "started",
+}, {
+  title: "Last Access",
+  dataIndex: "lastAccess",
+},
+{
+  title: "Expires",
+  dataIndex: "expires",
+},
+];
+
+const data = [{
+  key: "1",
+  ip: "134.156.177.188",
+  started : "Aug 2, 2018 3:48:24 AM",
+  lastAccess: "Aug 2, 2018 3:48:24 AM",
+  expires: "Aug 2, 2018 1:48:30 PM",
+},
+{
+  key: "1",
+  ip: "134.156.177.188",
+  started : "Aug 2, 2018 3:48:24 AM",
+  lastAccess: "Aug 2, 2018 3:48:24 AM",
+  expires: "Aug 2, 2018 1:48:30 PM",
+},
+{
+  key: "1",
+  ip: "134.156.177.188",
+  started : "Aug 2, 2018 3:48:24 AM",
+  lastAccess: "Aug 2, 2018 3:48:24 AM",
+  expires: "Aug 2, 2018 1:48:30 PM",
+}];
+
+class SessionsComponent extends React.Component<IProps, IState> {
+
+    constructor(props: IProps) {
+        super(props);
+    }
+
+    public render() {
+        return (
+            <div className="sessions">
+                <Table pagination={false} columns={columns} dataSource={data} size="small" />
+            </div >
+        );
+    }
+}
+
+export default SessionsComponent;
