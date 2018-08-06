01e88a9941cd51a32e8ce0974be6c259f7bbbcf2
Mahdy Beygi <mahdy@beygi.org>
2018-08-06 15:47:03 +0430

    add some gauge to admin dashboard

--- src/containers/Admin/Dashboard/index.tsx
+++ src/containers/Admin/Dashboard/index.tsx
@@ -1,13 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Col, Layout, Row } from "antd";
 import * as React from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import Balance from "../../../components/Balance";
 import Profile from "../../../components/DashboardHeaderProfile";
+import Gauge from "../../../components/Gauge";
 import Block from "../../../components/Holder";
 import Guide from "../../../components/UserStatusGuide";
-import {setUser} from "../../../redux/app/actions";
-import {IRootState} from "../../../redux/reducers";
+import { setUser } from "../../../redux/app/actions";
+import { IRootState } from "../../../redux/reducers";
 import t from "../../../services/trans/i18n";
 import "./style.less";
 
@@ -25,41 +26,113 @@ class AdminDashboardContainer extends React.Component<IProps, IState> {
     public render() {
         return (
             <Row gutter={8}>
-              <Col md={6} >
-                  <Block>
-                      <img src="https://dummyimage.com/600x400/4c4649/3ee6e0.png" alt=""/>
-                  </Block>
-                  <Block collapse>
-                          <img src="https://dummyimage.com/600x600/4c4649/3ee6e0.png" alt=""/>
-                  </Block>
-              </Col>
-              <Col md={18} >
-                  <Row gutter={8}>
+                <Col md={6} >
+                    <Block>
+                        <img src="https://dummyimage.com/600x400/4c4649/3ee6e0.png" alt="" />
+                    </Block>
+                    <Block collapse>
+                        <img src="https://dummyimage.com/600x600/4c4649/3ee6e0.png" alt="" />
+                    </Block>
+                </Col>
+                <Col md={18} >
+                    <Row gutter={8}>
+                        <Col md={24}>
+                            <Block>
+                            <Row gutter={8}>
+                                <Col md={6}>
+                                        BTC
+                                </Col>
+                                <Col md={6}>
+                                        <Gauge />
+                                </Col>
+                                <Col md={6}>
+                                        <Gauge />
+                                </Col>
+                                <Col md={6}>
+                                        <Gauge />
+                                </Col>
+                            </Row>
+                            </Block>
+                        </Col>
+                        <Col md={24}>
+                            <Block>
+                            <Row gutter={8}>
+                                <Col md={6}>
+                                        ETH
+                                </Col>
+                                <Col md={6}>
+                                        <Gauge />
+                                </Col>
+                                <Col md={6}>
+                                        <Gauge />
+                                </Col>
+                                <Col md={6}>
+                                        <Gauge />
+                                </Col>
+                            </Row>
+                            </Block>
+                        </Col>
+                        <Col md={24}>
+                            <Block>
+                            <Row gutter={8}>
+                                <Col md={6}>
+                                        LTC
+                                </Col>
+                                <Col md={6}>
+                                        <Gauge />
+                                </Col>
+                                <Col md={6}>
+                                        <Gauge />
+                                </Col>
+                                <Col md={6}>
+                                        <Gauge />
+                                </Col>
+                            </Row>
+                            </Block>
+                        </Col>
+                        <Col md={24}>
+                            <Block>
+                            <Row gutter={8}>
+                                <Col md={6}>
+                                        USD
+                                </Col>
+                                <Col md={6}>
+                                        <Gauge />
+                                </Col>
+                                <Col md={6}>
+                                        <Gauge />
+                                </Col>
+                                <Col md={6}>
+                                        <Gauge />
+                                </Col>
+                            </Row>
+                            </Block>
+                        </Col>
                         <Col md={6} >
-                            <Block title={t.t("Hot storage")} icon={<FontAwesomeIcon className="hot-storage" icon={["fab", "gripfire"] } /> } iconPosition="right">
+                            <Block title={t.t("Hot storage")} icon={<FontAwesomeIcon className="hot-storage" icon={["fab", "gripfire"]} />} iconPosition="right">
                                 <Balance />
                             </Block>
                         </Col>
                         <Col md={6} >
-                            <Block title={t.t("Cold storage")}  icon={<FontAwesomeIcon  className="cold-storage" icon={ ["fas", "snowflake"] } /> } iconPosition="right">
+                            <Block title={t.t("Cold storage")} icon={<FontAwesomeIcon className="cold-storage" icon={["fas", "snowflake"]} />} iconPosition="right">
                                 <Balance />
                             </Block>
                         </Col>
                         <Col md={6} >
-                            <Block title={t.t("Tax Cash Desk")}  icon={<FontAwesomeIcon  icon={ ["fas", "box"]  } /> }  iconPosition="right">
+                            <Block title={t.t("Tax Cash Desk")} icon={<FontAwesomeIcon icon={["fas", "box"]} />} iconPosition="right">
                                 <Balance />
                             </Block>
                         </Col>
                         <Col md={6} >
-                            <Block title={t.t("Fee Cash Desk")}   icon={<FontAwesomeIcon  icon={ ["fas", "box"] } /> }  iconPosition="right" >
+                            <Block title={t.t("Fee Cash Desk")} icon={<FontAwesomeIcon icon={["fas", "box"]} />} iconPosition="right" >
                                 <Balance />
                             </Block>
                         </Col>
-                  </Row>
-                   <Block>
-                       <img src="https://dummyimage.com/1000x300/4c4649/3ee6e0.png" alt=""/>
-                   </Block>
-              </Col>
+                    </Row>
+                    <Block>
+                        <img src="https://dummyimage.com/1000x300/4c4649/3ee6e0.png" alt="" />
+                    </Block>
+                </Col>
             </Row>
         );
     }
