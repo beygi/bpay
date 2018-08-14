ff24f8403468bdddff4d0ddc40b6752f4830f5fc
Mahdy Beygi <mahdy@beygi.org>
2018-08-14 16:49:10 +0430

    add document for translator service

--- src/services/trans/i18n.ts
+++ src/services/trans/i18n.ts
@@ -1,6 +1,15 @@
+/**
+ * @module i18n
+ */
 import * as i18next from "i18next";
 import config from "../../config";
 
+/**
+ * our translation class.
+ * po files converted to the i18next supported objects
+ * using webpack's po loader. stings in po files collected
+ * from srouce code using poedit
+ */
 i18next.init({
     interpolation: {
         // React already does escaping
@@ -10,9 +19,9 @@ i18next.init({
     lng: config.language.codeName, // 'en' | 'fa'
     nsSeparator: false,
     react:
-        {
-            wait: false,
-        },
+    {
+        wait: false,
+    },
     resources: {
         en: { translation: require("./en.po") },
         fa: { translation: require("./fa.po") },
