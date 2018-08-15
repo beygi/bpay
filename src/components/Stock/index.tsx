9106b340c42ab9a92c0d8c6d783362cb0cc6bc67
Mahdy Beygi <mahdy@beygi.org>
2018-08-15 12:35:46 +0430

    add doc for stock component

--- src/components/Stock/index.tsx
+++ src/components/Stock/index.tsx
@@ -13,18 +13,23 @@ import t from "../../services/trans/i18n";
 import "./style.less";
 
 interface IProps {
+    /**  selected symbol */
     symbol: string;
+    /**  crypto currencies data from redux */
     cryptos: any[];
+    /**  forex data from redux */
     forex: any[];
+    /**  don't dispay title and icon if sets to true, useful for using in collapsable blocks */
     hideTitle?: boolean;
 }
 
 interface IState {
+    /**  holds exchange rate data to display */
     exchangeRates: any[];
 }
 
+/** calcucalte exchange rate from store */
 function calcExchangeRate(props) {
-    // calcucalte exchange rate from store
     const exchange = [
         { symbol: "EUR", rate: 0 },
         { symbol: "USD", rate: 0 },
@@ -36,20 +41,23 @@ function calcExchangeRate(props) {
     // calc exchange rate
     const calculated = exchange.map((fiat) => {
         fiat.rate = _.round(props.cryptos[props.symbol].quotes.USD.price * props.forex[fiat.symbol], 2);
-        if (fiat.symbol === "IRR") { fiat.rate = _.round( fiat.rate, 0); }
+        if (fiat.symbol === "IRR") { fiat.rate = _.round(fiat.rate, 0); }
         return fiat;
     });
     return calculated;
 }
 
+/**
+ * display a currency price in fiat
+ */
 class StockComponent extends React.Component<IProps, IState> {
 
     public static getDerivedStateFromProps(props, state) {
-            // update state.balance when props changes by redux4
-            if (props.cryptos !== null && props.forex !== null) {
-                 return {  exchangeRates : calcExchangeRate(props)};
-            }
-            return null;
+        // update state.balance when props changes by redux
+        if (props.cryptos !== null && props.forex !== null) {
+            return { exchangeRates: calcExchangeRate(props) };
+        }
+        return null;
     }
 
     constructor(props: IProps) {
@@ -67,31 +75,31 @@ class StockComponent extends React.Component<IProps, IState> {
         if (!this.props.cryptos || !this.props.forex) {
             return (<div>Loading ...</div>);
         }
-        const rates = this.state.exchangeRates.map( (rate) => {
+        const rates = this.state.exchangeRates.map((rate) => {
             return <div key={rate.symbol}><span className="symbol">{rate.symbol}: </span><span className="rate"><Ex value={rate.rate} seperateThousand /></span></div>;
-        } );
-        const title = (this.props.hideTitle) ? null : <span className="balance-name"><i className={`live-icon cc ${this.props.symbol}`}></i>{this.props.cryptos[this.props.symbol].name}</span> ;
+        });
+        const title = (this.props.hideTitle) ? null : <span className="balance-name"><i className={`live-icon cc ${this.props.symbol}`}></i>{this.props.cryptos[this.props.symbol].name}</span>;
         return (
             <div className="stock" >
                 {title}
-                    <div className="rates">
-                        {rates}
-                    </div>
+                <div className="rates">
+                    {rates}
+                </div>
             </div>
         );
     }
 }
 
 function mapStateToProps(state: IRootState) {
-    if            (state.app.market && state.app.market.cryptos && state.app.market.forex) {
+    if (state.app.market && state.app.market.cryptos && state.app.market.forex) {
         return {
-            cryptos:            state.app.market.cryptos,
+            cryptos: state.app.market.cryptos,
             forex: state.app.market.forex,
         };
     }
     // there is no balance from redux, state must not be updated in getDerivedStateFromProps
     return {
-        cryptos:       null,
+        cryptos: null,
         forex: null,
     };
 }
