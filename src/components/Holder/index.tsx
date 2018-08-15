6e904b7e442dae6d73fc5d08776dc0590735cbb0
Mahdy Beygi <mahdy@beygi.org>
2018-08-15 11:01:50 +0430

    beautify holder

--- src/components/Holder/index.tsx
+++ src/components/Holder/index.tsx
@@ -48,11 +48,11 @@ class BlockComponent extends React.Component<IProps, IState> {
     public render() {
         let cssClass = this.props.className || "";
         const transparentClass = (this.props.transparent) ? "transparent" : "";
-        let title: JSX.Element ;
+        let title: JSX.Element;
         const icon: JSX.Element = (this.props.icon) ? <span className={`${this.props.iconPosition || "left"}`}>{this.props.icon}</span> : null;
         const disabled = (this.props.collapseClosed) ? ["0"] : ["1"];
-        const showArrow = (this.props.showArrow !== undefined) ?  this.props.showArrow : true;
-        const centerTitle = (this.props.centerTitle !== undefined && this.props.centerTitle) ?  "center-title" : "";
+        const showArrow = (this.props.showArrow !== undefined) ? this.props.showArrow : true;
+        const centerTitle = (this.props.centerTitle !== undefined && this.props.centerTitle) ? "center-title" : "";
 
         // handle no-padding block
         cssClass += " block";
@@ -69,22 +69,22 @@ class BlockComponent extends React.Component<IProps, IState> {
         if (this.props.collapse) {
             title = <div className="block-title-collapse">{icon} {this.props.title || <span>&nbsp;</span>}</div>;
             return (
-            <div className={cssClass} >
-                        <Collapse key={disabled[0]} className= "block-collapse" bordered={false} defaultActiveKey={["1"]}   >
-                                  <Panel header={title}  key="1" showArrow={showArrow} >
-                                      {this.props.children}
-                                  </Panel>
-                        </Collapse>
-            </div>
+                <div className={cssClass} >
+                    <Collapse key={disabled[0]} className="block-collapse" bordered={false} defaultActiveKey={["1"]}   >
+                        <Panel header={title} key="1" showArrow={showArrow} >
+                            {this.props.children}
+                        </Panel>
+                    </Collapse>
+                </div>
             );
         }
 
         title = (this.props.title) ? <div className={`block-title${titleCssClass}`}>{icon}<span className={centerTitle}>{this.props.title}</span></div> : null;
         return (
-        <div className={cssClass} >
-                    {title}
-                    {this.props.children}
-        </div >
+            <div className={cssClass} >
+                {title}
+                {this.props.children}
+            </div >
         );
     }
 
