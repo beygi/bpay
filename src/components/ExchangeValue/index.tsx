b611699272c6df1d41eac6d1e88aedd7df4e6385
Mahdy Beygi <mahdy@beygi.org>
2018-08-15 09:31:50 +0430

    add doc for exchange value component

--- src/components/ExchangeValue/index.tsx
+++ src/components/ExchangeValue/index.tsx
@@ -6,16 +6,26 @@ const commaNumber = require("comma-number");
 import "./style.less";
 
 interface IProps {
+    /** input numer */
     value: number;
+    /**  number of floating point digits to display , default is 2 */
     fixFloatNum?: number;
+    /** seperate thousands with comma */
     seperateThousand?: boolean;
 }
 
 interface IState {
+    /** keeps value to compare with new one from props */
     value: number;
+    /** growth status */
     status?: "grow" | "fall" | "normal";
 }
 
+/**
+ * this component display numbers and support colorful display
+ * when number grows or falls, thousand seperator and fix floats
+ * are supported too
+ */
 class ExchangeValueComponent extends React.Component<IProps, IState> {
 
     public static getDerivedStateFromProps(props, state) {
