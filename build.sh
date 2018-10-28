39ea0a15cb89cc289537a423c19ef1efb429b070
Mahdy Beygi <mahdyb@gmail.com>
2018-10-28 15:11:04 +0330

    fix build scrip. refs #654

--- build.sh
+++ build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+BLUE='\033[1;34m'
+NC='\033[0m' # No Color
+set -e
 echo "###### date ######"
 date +%d\ %m\ %Y
 echo "###### whoami ######"
 whoami
 echo "###### cd to src dir ######"
-cd /home/staging/web
+#cd /home/staging/web
 pwd
 echo "###### last commit message ######"
-git log -1 | cat
+comment="$(git log -1)"
+#echo "${comment}"
+printf "I ${BLUE}${comment}${NC}\n"
 echo "###### remove dist dir ######"
-rm -R dist
+rm -r -f dist
 echo "###### install dev dependencies ######"
 yarn install
 echo "###### build ... ######"
@@ -17,6 +22,5 @@ yarn build
 echo "###### Replace old created files with new ones ######"
 mv out todelete
 mv dist out
-rm -R todelete
+rm -r todelete
 echo "###### DONE ######"
-
