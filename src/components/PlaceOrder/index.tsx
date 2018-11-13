1df625666d5757fe3f8d55f1853fb515b9fccf79
Mahdy Beygi <mahdyb@gmail.com>
2018-11-13 12:16:42 +0330

    add input numbers, validation and calculate sum in place order block

--- src/components/PlaceOrder/index.tsx
+++ src/components/PlaceOrder/index.tsx
@@ -1,13 +1,14 @@
 /**
  * @module Components/PlaceOrderComponent
  */
-import { Button, Checkbox, Col, Form, Input, Row, Tabs } from "antd";
+import { Button, Checkbox, Col, Form, Input, InputNumber, Row, Tabs } from "antd";
 import { FormComponentProps } from "antd/lib/form";
+import * as _ from "lodash";
 import * as React from "react";
-import { connect } from "react-redux";
+import Ex from "../../components/ExchangeValue";
 import Block from "../../components/Holder";
 import config from "../../config";
-import { IRootState } from "../../redux/reducers";
+import Tools from "../../services/tools";
 import t from "../../services/trans/i18n";
 import "./style.less";
 
@@ -33,6 +34,8 @@ interface IPlaceProps extends FormComponentProps {
 }
 
 interface IState {
+    /** holds total value of order */
+    total?: number;
 }
 
 /**
@@ -42,13 +45,64 @@ class PlaceOrderComponent extends React.Component<IPlaceProps, IState> {
 
     constructor(props: IPlaceProps) {
         super(props);
+        let floatNumbers = 2;
+        if (this.props.toSymbol === "IRR") {
+            floatNumbers = 0;
+        }
+        if (this.props.toSymbol === "BTC" || this.props.toSymbol === "ETH") {
+            floatNumbers = 4;
+        }
+        this.state = {
+            total: _.round(Tools.getPrice(this.props.fromSymbol, this.props.toSymbol), floatNumbers),
+        };
+
+        this.handlePriceChange = this.handlePriceChange.bind(this);
+        this.handleAmountChange = this.handleAmountChange.bind(this);
+    }
+
+    public componentDidUpdate(prevProps) {
+        // Typical usage (don't forget to compare props):
+        if (this.props.fromSymbol !== prevProps.fromSymbol || this.props.toSymbol !== prevProps.toSymbol) {
+            let floatNumbers = 2;
+            if (this.props.toSymbol === "IRR") {
+                floatNumbers = 0;
+            }
+            if (this.props.toSymbol === "BTC" || this.props.toSymbol === "ETH") {
+                floatNumbers = 4;
+            }
+            this.props.form.setFieldsValue({
+                price: _.round(Tools.getPrice(this.props.fromSymbol, this.props.toSymbol), floatNumbers),
+                amount: 1,
+            });
+            this.setState({ total: _.round(Tools.getPrice(this.props.fromSymbol, this.props.toSymbol), floatNumbers) });
+        }
     }
 
     public handleSubmit = (e) => {
         e.preventDefault();
-
+        console.log(Tools.getPrice(this.props.fromSymbol, this.props.toSymbol));
     }
+
+    public handlePriceChange(value) {
+        this.setState({ total: this.props.form.getFieldValue("amount") * value });
+    }
+
+    public handleAmountChange(value) {
+        this.setState({ total: this.props.form.getFieldValue("price") * value });
+    }
+
     public render() {
+        let step = 0.1;
+        let floatNumbers = 2;
+        if (this.props.toSymbol === "IRR") {
+            step = 10000;
+            floatNumbers = 0;
+        }
+        if (this.props.toSymbol === "BTC" || this.props.toSymbol === "ETH") {
+            step = 0.001;
+            floatNumbers = 4;
+        }
+
         const { getFieldDecorator } = this.props.form;
         return (
             <Tabs defaultActiveKey="1" >
@@ -57,27 +111,43 @@ class PlaceOrderComponent extends React.Component<IPlaceProps, IState> {
                         <Col md={12}>
                             <Block className="place-order">
                                 <h3>{t.t("Buy ") + t.t(config.currencies[this.props.fromSymbol].name)}</h3>
-                                <Form onSubmit={this.handleSubmit} className="login-form">
+                                <Form onSubmit={this.handleSubmit} className="buy-form">
                                     <FormItem {...formItemLayout} label={t.t("Price")}>
                                         {getFieldDecorator("price", {
-                                            rules: [{ required: true, message: "Please input price" }],
+                                            initialValue: _.round(Tools.getPrice(this.props.fromSymbol, this.props.toSymbol), floatNumbers),
+                                            rules: [{
+                                                required: true,
+                                                message: t.t("Please input price"),
+                                            }],
                                         })(
-                                            <Input />,
+                                            <InputNumber
+                                                placeholder={this.props.toSymbol}
+                                                min={0}
+                                                max={10000000000}
+                                                step={step}
+                                                onChange={this.handlePriceChange}
+                                                formatter={(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                                            />,
                                         )}
                                     </FormItem>
                                     <FormItem {...formItemLayout} label={t.t("Amount")}>
                                         {getFieldDecorator("amount", {
-                                            rules: [{ required: true, message: "Please input amount" }],
+                                            initialValue: 1,
+                                            rules: [{
+                                                required: true,
+                                            }],
                                         })(
-                                            <Input />,
+                                            <InputNumber
+                                                onChange={this.handleAmountChange}
+                                                placeholder={this.props.fromSymbol}
+                                                min={0}
+                                                max={10000000000}
+                                                formatter={(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                                            />,
                                         )}
                                     </FormItem>
                                     <FormItem {...formItemLayout} label={t.t("Total")}>
-                                        {getFieldDecorator("total", {
-                                            rules: [{ required: false, message: "Please input amount" }],
-                                        })(
-                                            <Input />,
-                                        )}
+                                        <Ex fixFloatNum={floatNumbers} value={this.state.total} stockStyle={false} />
                                     </FormItem>
                                     <FormItem>
                                         <Button className="buy-btn" type="primary" htmlType="submit" >
@@ -88,37 +158,6 @@ class PlaceOrderComponent extends React.Component<IPlaceProps, IState> {
                             </Block>
                         </Col>
                         <Col md={12}>
-                            <Block className="place-order">
-                                <h3>{t.t("Sell ") + t.t(config.currencies[this.props.fromSymbol].name)}</h3>
-                                <Form onSubmit={this.handleSubmit} className="login-form">
-                                    <FormItem {...formItemLayout} label={t.t("Price")}>
-                                        {getFieldDecorator("price", {
-                                            rules: [{ required: true, message: "Please input price" }],
-                                        })(
-                                            <Input />,
-                                        )}
-                                    </FormItem>
-                                    <FormItem {...formItemLayout} label={t.t("Amount")}>
-                                        {getFieldDecorator("amount", {
-                                            rules: [{ required: true, message: "Please input amount" }],
-                                        })(
-                                            <Input />,
-                                        )}
-                                    </FormItem>
-                                    <FormItem {...formItemLayout} label={t.t("Total")}>
-                                        {getFieldDecorator("total", {
-                                            rules: [{ required: false, message: "Please input amount" }],
-                                        })(
-                                            <Input />,
-                                        )}
-                                    </FormItem>
-                                    <FormItem>
-                                        <Button className="sell-btn" type="primary" htmlType="submit" >
-                                            {t.t("Sell ") + t.t(config.currencies[this.props.fromSymbol].name)}
-                                        </Button>
-                                    </FormItem>
-                                </Form>
-                            </Block>
                         </Col>
                     </Row>
                 </TabPane>
