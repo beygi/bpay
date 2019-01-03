6e9f78e5f129296c82331ffec869943f71d6d203
Mahdy Beygi <mahdyb@gmail.com>
2019-01-03 17:17:59 +0330

    fix theme,inactive style and local numbers for chart. refs #835

--- src/components/LineChart/index.tsx
+++ src/components/LineChart/index.tsx
@@ -5,11 +5,16 @@ import ReactEcharts from "echarts-for-react";
 import * as _ from "lodash";
 import * as Moment from "moment";
 import * as React from "react";
+import { connect } from "react-redux";
+import { IRootState } from "../../redux/reducers";
+import t from "../../services/trans/i18n";
 import "./style.less";
 
 interface IProps {
     /** guage title with is displayed in center of guage */
     title: string;
+    /** user selected theme */
+    theme: string;
 }
 
 interface IState {
@@ -71,10 +76,12 @@ class LineChartComponent extends React.Component<IProps, IState> {
                 show: true,
                 top: 10,
                 height: 20,
+                inactiveColor: "#66666680",
                 textStyle:
                 {
                     verticalAlign: "bottom",
                     height: 50,
+                    fontWeight: "bold",
                     fontFamily: "b2mark, Becopay",
                 },
             },
@@ -87,6 +94,7 @@ class LineChartComponent extends React.Component<IProps, IState> {
                         margin: 20,
                         align: "center",
                         fontFamily: "b2mark, Becopay",
+                        fontWeight: "bold",
                         formatter: ((value) => {
                             return Moment(value).format("DD MMMM YY");
                         }),
@@ -101,7 +109,8 @@ class LineChartComponent extends React.Component<IProps, IState> {
                         margin: 20,
                         align: "center",
                         fontFamily: "b2mark, Becopay",
-                        formatter: ((value) => value),
+                        fontWeight: "bold",
+                        formatter: ((value) => value.toLocaleString(t.default.language, { useGrouping: true })),
                     },
                 },
             ],
@@ -112,7 +121,7 @@ class LineChartComponent extends React.Component<IProps, IState> {
         return (
             <div className="">
                 <ReactEcharts
-                    theme="light"
+                    theme={this.props.theme}
                     option={options}
                 />
             </div>
@@ -120,4 +129,11 @@ class LineChartComponent extends React.Component<IProps, IState> {
     }
 }
 
-export default LineChartComponent;
+function mapStateToProps(state: IRootState) {
+    return {
+        theme: state.app.user.theme,
+        language: state.app.user.language,
+    };
+}
+
+export default connect(mapStateToProps)(LineChartComponent);
